a2831ebee897cb06d6d5b85aac75aaffe30a6f45
mmbbddll <erehwonmi@proton.me>
2025-12-10 22:02:39 +0800

    done answering exer 1

--- 0-limit-crawler/main.go
+++ 0-limit-crawler/main.go
@@ -12,7 +12,13 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
+
+// my idea is to create a global channel that acts as a gate pass holder, this gate pass holder can obtain a pass every second
+// and then pass this 'pass' to a Crawl goroutine (whoever Crawl goroutine that can first grab it wins, while other goroutines will be blocked from fetching)
+
+var globalChannel = make(chan bool, 1)
 
 // Crawl uses `fetcher` from the `mockfetcher.go` file to imitate a
 // real crawler. It crawls until the maximum depth has reached.
@@ -22,6 +28,9 @@ func Crawl(url string, depth int, wg *sync.WaitGroup) {
 	if depth <= 0 {
 		return
 	}
+
+	// block other go routines from executing except for 1 that can obtain the pass
+	<- globalChannel
 
 	body, urls, err := fetcher.Fetch(url)
 	if err != nil {
@@ -41,6 +50,16 @@ func Crawl(url string, depth int, wg *sync.WaitGroup) {
 
 func main() {
 	var wg sync.WaitGroup
+
+	go func(){
+		ticker := time.NewTicker(1 * time.Second)
+		defer ticker.Stop()
+
+		for range ticker.C {
+			// hand over a pass to global channel every sec
+			globalChannel <- true
+		}
+	}()
 
 	wg.Add(1)
 	Crawl("http://golang.org/", 4, &wg)
